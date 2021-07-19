12305110afb0ca8ad5f0866a1d8209cd0a6d096b
dairof7 <dairof7@gmail.com>
2021-07-19 18:33:35 -0500

    [update] change logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,11 +6,10 @@ canUnlockAll module
 
 def canUnlockAll(boxes):
     num_boxes = len(boxes)
-    keys = boxes[0]
-    keys.append(0)
+    keys = [0] + boxes[0]
     idx_key = 1
     for i in range(num_boxes-1):
-        for j in range(idx_key, len(keys) + 1):
+        for j in keys[1:]:
             keys = list(set(keys + boxes[j]))
             idx_key += 1
             i += 1
