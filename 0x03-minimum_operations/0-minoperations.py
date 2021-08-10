b295ce753cdd4010a62de9e9818fd1567210b01b
dairo <dairof7@gmail.com>
2021-08-09 21:16:11 -0500

    [upload] change variable name

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -14,14 +14,14 @@ def minOperations(n):
         [int]: [minimun operations]
     """
     i = int(n/2)
-    ac = 0
+    num_operations = 0
     num = n
     while num > 1:
         while num % i != 0:
             i -= 1
             if i == 0:
                 return n
-        ac += num / i
+        num_operations += num / i
         num = num/(num / i)
         i -= 1
-    return int(ac)
+    return int(num_operations)
