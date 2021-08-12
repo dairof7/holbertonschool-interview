9727bf504bd9aab3ed398072f34527fbe1eb699e
dairo <dairof7@gmail.com>
2021-08-12 11:42:52 -0500

    [update] optimize all

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,21 +7,18 @@ to create a certain number of characters.
 
 def minOperations(n):
     """
-    Min Operations
-    Args:
-        n (int): number of copies
-    Returns:
-        [int]: [minimun operations]
+    Minimum Operations
     """
-    i = int(n/2)
+
+    if n <= 1:
+        return 0
+    div = 2 
     num_operations = 0
-    num = n
-    while num > 1:
-        while num % i != 0:
-            i -= 1
-            if i == 0:
-                return n
-        num_operations += num / i
-        num = num / (num / i)
-        i -= 1
-    return int(num_operations)
+
+    while n > 1:
+        if n % div == 0:
+            n /= div
+            num_operations += div
+        else:
+            div += 1
+    return num_operations
