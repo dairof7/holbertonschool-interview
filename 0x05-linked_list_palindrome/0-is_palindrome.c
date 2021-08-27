7b54f5800e4a1a3fcba8619a994eceeb347c5363
dairo <dairof7@gmail.com>
2021-08-26 21:24:58 -0500

    fix

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,11 +1,15 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * analyze -check if  is palindrome
  * @head1: pointer to the head of the linked list
  * @head2: pointer to the head of the linked list
+ * Description: we use head2 to get to the end
+ * using recursion. Then we compare head1
+ * with the current value of head2. Then we compare head 1 with the
+ * current value of head2. Finally we start increasing the position of
+ * head1 to compare it with the old values of head2. This has the
+ * effect of comparing start + 1 with end-1.
  * Return: 1 (palindrome) || 0
  */
 static int analyze(listint_t **head1, listint_t *head2)
@@ -28,6 +32,7 @@ static int analyze(listint_t **head1, listint_t *head2)
 /**
  * is_palindrome - checks if a linked list with int is a palindrome
  * @head: head pointer to the linked list
+ *
  * Return: 1 (palindrome) || 0
  */
 int is_palindrome(listint_t **head)
