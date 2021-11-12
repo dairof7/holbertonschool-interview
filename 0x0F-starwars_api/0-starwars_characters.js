634063b287ca608684e4071e9ffe8a4449cd7801
dairof7 <dairoff@gmail.com>
2021-11-11 21:54:14 -0500

    [fix]

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,17 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    recursionRequestPrint(characters, 0);
-  }
-});
 
-function recursionRequestPrint (url, index) {
-  request(url[index], (err, res, body) => {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+const request = require('request');
+
+function filmRequest (url) {
+  request(url, async function (error, response, body) {
+    for (const charURL of JSON.parse(body).characters) {
+      if (error) {
+        console.log(`Error: ${error}`);
+        return;
+      }
+      const charJSON = await requestChar(charURL);
+      console.log(JSON.parse(charJSON).name);
     }
   });
 }
+
+function requestChar (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) reject(error);
+      else resolve(body);
+    });
+  });
+}
+
+filmRequest(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`);
